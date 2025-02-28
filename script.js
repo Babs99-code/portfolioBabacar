d211f8f7805022a8d269f21fa56e9e838e6c6cdd
Babs99-code <boubasarr47@gmail.com>
2025-02-28 18:14:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+function openModal(img) {
+    document.getElementById("imageModal").style.display = "flex";
+    document.getElementById("modalImage").src = img.src;
+    document.getElementById("modalText").innerText = img.alt;
+}
+
+function closeModal() {
+    document.getElementById("imageModal").style.display = "none";
+}
 
